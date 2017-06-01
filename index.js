03a732d951876c769020a3559f086091c8ccee65
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-01 19:19:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
     for (let i = 1; i <= 16; i++) {
         pdfFiles.push(base + i + ".pdf");
     }
-    let pdfMerge = PDFMerge(pdfFiles,"newspaper.pdf",(function (error) {
+    let pdfMerge = PDFMerge(pdfFiles,"newspaper.pdf",function (error) {
             if (error)
                 res.end(error + "\n" + error.stack);
             res.sendfile("newspaper.pdf");
